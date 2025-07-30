7b5863c538afb93b03e775065023ac7feaccd96c
TrucLam <httlam2004ntkt@gmail.com>
2025-07-31 00:06:10 +0700

    Update project mock data and styles for Home component
    - Changed project name from "Noir" to "Yup" and updated associated images in projectsMock.
    - Added a new project "Melody" with relevant details to projectsMock.
    - Updated background color in Home.module.scss for improved visual consistency.

--- src/services/projects.tsx
+++ src/services/projects.tsx
@@ -12,12 +12,12 @@ export type Project = {
 export const projectsMock: Project[] = [
   {
     id: "1",
-    name: "Noir",
+    name: "Yup",
     time: "Sep 10, 2024",
     image:
       "https://ik.imagekit.io/mku5dcybr/Yup!%20Everything%20on%20Your%20Phone!.svg?updatedAt=1753888300585",
     subImage:
-      "https://static.wixstatic.com/media/bb0466_b29ff5e5635748d8a412656cf9a09bc2~mv2.png/v1/fill/w_1310,h_754,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/Noir.png",
+      "https://ik.imagekit.io/mku5dcybr/Yup!%20Everything%20on%20Your%20Phone!.svg?updatedAt=1753888300585",
     description:
       "Lorem Ipsum is siLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever",
     linkToLive: "https://www.google.com",
@@ -31,6 +31,17 @@ export const projectsMock: Project[] = [
       "https://static.wixstatic.com/media/bb0466_b29ff5e5635748d8a412656cf9a09bc2~mv2.png/v1/fill/w_1310,h_754,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/Noir.png",
     subImage:
       "https://static.wixstatic.com/media/bb0466_b29ff5e5635748d8a412656cf9a09bc2~mv2.png/v1/fill/w_1310,h_754,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/Noir.png",
+    description:
+      "Lorem Ipsum is siLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever",
+    linkToLive: "https://www.google.com",
+    linkToFigma: "https://www.google.com",
+  },
+  {
+    id: "3",
+    name: "Melody",
+    time: "Feb 01, 2025",
+    image: "https://ik.imagekit.io/mku5dcybr/Me.png?updatedAt=1753894222146",
+    subImage: "https://ik.imagekit.io/mku5dcybr/Me.png?updatedAt=1753894222146",
     description:
       "Lorem Ipsum is siLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever",
     linkToLive: "https://www.google.com",
